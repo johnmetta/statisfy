855d90118a694a54e5ec8a58e4299a74fe3f1f5a
Anton Gogolev <anton.gogolev@hglabhq.com>
2014-08-19 09:18:10 +0400

    Statsify.Core: Rewriting Annotation Database

---
+++ src/Statsify.Core/Storage/Annotation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Statsify.Core.Storage
+{
+    [DebuggerDisplay("{Timestamp}: {Title,nq}")]
+    public class Annotation
+    {
+        public DateTime Timestamp { get; private set; }
+
+        public string Title { get; private set; }
+ 
+        public string Message { get; private set; }
+
+        public Annotation(DateTime timestamp, string title, string message)
+        {
+            Timestamp = timestamp;
+            Title = title;
+            Message = message;
+        }
+    }
+}
