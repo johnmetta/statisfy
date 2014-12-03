078785a8b9894dcab6f343d0b17fe62d36127dd4
Anton Gogolev <anton.gogolev@aeroclub.ru>
2014-12-03 19:23:48 +0300

    Statsify.Tests

---
+++ src/Statsify.Tests/Agent/Impl/MetricDefinitionFactoryTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Statsify.Agent.Configuration;
+using Statsify.Agent.Impl;
+
+namespace Statsify.Tests.Agent.Impl
+{
+    [TestFixture]
+    public class MetricDefinitionFactoryTests
+    {
+        [Test]
+        public void CreateInstance()
+        {
+            var metricDefinition = new MetricDefinitionFactory().CreateInstance(new MetricConfigurationElement {
+                Name = "sql_server.lock_timeouts_sec",
+                Type = "performance-counter",
+                Path = @"\SQLServer:Locks(_Total)\Lock Timeouts/sec",
+                AggregationStrategy = AggregationStrategy.Gauge
+            });
+        }
+    }
+}
