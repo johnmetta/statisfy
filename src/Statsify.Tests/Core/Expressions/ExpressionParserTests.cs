710d6f46a10c655185c5455f9e71c9e3d42da5ad
Anton Gogolev <anton.gogolev@aeroclub.ru>
2014-12-29 17:07:02 +0300

    Test crap

--- src/Statsify.Tests/Core/Expressions/ExpressionParserTests.cs
+++ src/Statsify.Tests/Core/Expressions/ExpressionParserTests.cs
@@ -17,13 +17,16 @@ namespace Statsify.Tests.Core.Expressions
 
             var parser = new ExpressionParser();
             var expression = parser.Parse(new TokenStream(tokens));
+            
+            tokens = scanner.Scan("alias_by_fragment(ema(servers.srv-aps3.system.processor.{total*,user*}, 50), 2, 4)");
+            expression = parser.Parse(new TokenStream(tokens));
 
             Environment.RegisterFunctions(typeof(Functions));
 
-            var metricRegistry = new MetricRegistry(@"c:\statsify");
+            //var metricRegistry = new MetricRegistry(@"c:\statsify");
 
-            var env = new Environment { MetricRegistry = metricRegistry };
-            var r = expression.Evaluate(env, new EvalContext(DateTime.UtcNow.AddMinutes(-20), DateTime.UtcNow));
+            //var env = new Environment { MetricRegistry = metricRegistry };
+            //var r = expression.Evaluate(env, new EvalContext(DateTime.UtcNow.AddMinutes(-20), DateTime.UtcNow));
         }
     }
 }
