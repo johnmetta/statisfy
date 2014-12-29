821c590d8753ee9de10f8c748bb7b46478c060bb
Anton Gogolev <anton.gogolev@aeroclub.ru>
2014-12-29 17:05:46 +0300

    Graphite-compatible DateTime parser

--- src/Statsify.Web.Api/Models/SeriesQueryModel.cs
+++ src/Statsify.Web.Api/Models/SeriesQueryModel.cs
@@ -8,4 +8,13 @@
 
         public string[] Expression { get; set; }
     }
+
+    public class AnnotationsQueryModel
+    {
+        public string From { get; set; }
+
+        public string Until { get; set; }
+
+        public string[] Tag { get; set; }
+    }
 }
