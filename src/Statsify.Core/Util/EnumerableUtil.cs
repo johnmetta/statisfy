f276653b7d7ce3cbfc30e8f7948a7471c3d24aaa
Anton Gogolev <anton.gogolev@aeroclub.ru>
2015-01-12 09:49:57 +0300

    Functions: New and improved

---
+++ src/Statsify.Core/Util/EnumerableUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Statsify.Core.Util
+{
+    public static class EnumerableUtil
+    {
+        public static IEnumerable<T> Generate<T>(T seed, Func<T, T> nextValue, Predicate<T> @while)
+        {
+            var value = seed;
+            while(@while(value))
+            {
+                yield return value;
+                value = nextValue(value);
+            } // while
+        }
+    }
+}
