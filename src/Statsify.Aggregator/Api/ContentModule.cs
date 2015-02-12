3a4018743ce7d274c991bd1a0bd3784ad222b89e
Anton Gogolev <anton.gogolev@aeroclub.ru>
2015-02-12 13:41:04 +0300

    Add D3 v3.4.11 and MetricsGraphics v2.1

---
+++ src/Statsify.Aggregator/Api/ContentModule.cs
@@ -0,0 +1,25 @@
+﻿using Nancy;
+
+namespace Statsify.Aggregator.Api
+{
+    public class ContentModule : NancyModule
+    {
+        public ContentModule()
+        {
+            Get["/content/statsify.css"] = GetStatsifyCss;
+
+            Get["/content/statsify.js"] = r => GetStatsifyJs(r, false);
+            Get["/content/statsify.min.js"] = r => GetStatsifyJs(r, true);
+        }
+
+        private object GetStatsifyCss(dynamic r)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private object GetStatsifyJs(dynamic r, bool min)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
