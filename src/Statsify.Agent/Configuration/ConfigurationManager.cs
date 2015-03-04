cfd7d163024ded8eb2de5bb3d504b9ed5ea11151
Anton Gogolev <anton.gogolev@aeroclub.ru>
2015-03-04 20:12:46 +0300

    Statsify.Agent: Support for several configuration file locations

--- src/Statsify.Agent/Configuration/ConfigurationManager.cs
+++ src/Statsify.Agent/Configuration/ConfigurationManager.cs
@@ -1,35 +1,35 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Xml;
 
 namespace Statsify.Agent.Configuration
 {
     public class ConfigurationManager
     {
-        private static readonly string Path;
-
-        static ConfigurationManager()
-        {
-            Path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
-                "Statsify.Agent", "statsify-agent.config");
-
-#if DEBUG
-            Path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "statsify-agent.config");
-#endif
-
-        }
-
         public StatsifyAgentConfigurationSection Configuration { get; private set; }
 
-        public ConfigurationManager() :
-            this(Path) { }
+        public static ConfigurationManager Instance
+        {
+            get
+            {
+                var configurationFilePaths = new[] {
+                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Statsify", "Agent", "statsify-agent.config"),
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "statsify-agent.config")
+                };
 
-        public ConfigurationManager(string configurationFilePath)
+                var configurationFilePath = configurationFilePaths.FirstOrDefault(File.Exists);
+                if(configurationFilePath == null) throw new ApplicationException();
+
+                return new ConfigurationManager(configurationFilePath);
+            }
+        }
+
+        private ConfigurationManager(string configurationFilePath)
         {
             using(var stream = File.OpenRead(configurationFilePath))
             {
                 var xmlDocument = new XmlDocument();
-
                 xmlDocument.Load(stream);
 
                 if(xmlDocument.DocumentElement == null) return;
@@ -37,7 +37,6 @@ namespace Statsify.Agent.Configuration
                 using(var xmlReader = new XmlNodeReader(xmlDocument.DocumentElement))
                 {
                     var configuration = new StatsifyAgentConfigurationSection(xmlReader);
-
                     Configuration = configuration;
                 }
             }
