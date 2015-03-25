ce3af5d8d5a04e99df9d4e6f677fb670fd5d2bfa
Anton Gogolev <anton.gogolev@aeroclub.ru>
2015-03-25 09:43:53 +0300

    Statsify.Aggregator: Create `statsify-aggregator.config` on startup if it's missing

--- src/Statsify.Aggregator/Configuration/ConfigurationManager.cs
+++ src/Statsify.Aggregator/Configuration/ConfigurationManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Xml;
@@ -7,19 +8,47 @@ namespace Statsify.Aggregator.Configuration
 {
     public class ConfigurationManager
     {
+        private const string ConfigurationFileName = "statsify-aggregator.config";
+
         public StatsifyAggregatorConfigurationSection Configuration { get; private set; }
 
         public static ConfigurationManager Instance
         {
             get
             {
+                var programDataDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Statsify", "Aggregator");
+                var programDataConfigurationFilePath = Path.Combine(programDataDirectoryPath, ConfigurationFileName);
+
                 var configurationFilePaths = new[] {
-                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "statsify-aggregator.config"),
-                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Statsify", "Aggregator", "statsify-aggregator.config"),
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigurationFileName),
+                    programDataConfigurationFilePath
                 };
 
                 var configurationFilePath = configurationFilePaths.FirstOrDefault(File.Exists);
-                if(configurationFilePath == null) throw new ApplicationException();
+                if(configurationFilePath == null)
+                {
+                    if(!Directory.Exists(programDataDirectoryPath))
+                        Directory.CreateDirectory(programDataDirectoryPath);
+
+                    using(var stream = typeof(ConfigurationManager).Assembly.GetManifestResourceStream("Statsify.Aggregator." + ConfigurationFileName))
+                    {
+                        if(stream == null) throw new Exception(); // FIXME
+
+                        using(var streamReader = new StreamReader(stream))
+                        {
+                            var configuration = streamReader.ReadToEnd();
+                            configuration = configuration.Replace("{{ storage-path }}", 
+                                Path.Combine(programDataDirectoryPath, "Data"));
+
+                            using(var fileStream = File.Create(programDataConfigurationFilePath))
+                            using(var streamWriter = new StreamWriter(fileStream))
+                                streamWriter.Write(configuration);
+                        } // using
+                    } // using
+
+                    configurationFilePath = programDataConfigurationFilePath;
+                } // if
+
 
                 return new ConfigurationManager(configurationFilePath);
             }
