09d47e411f701f4b0125bcbe027a93c0c5747c8a
Anton Gogolev <anton.gogolev@hglabhq.com>
2015-04-10 18:17:24 +0300

    Remove Aeroclub-related fragments

--- src/Statsify.Tests/Core/Storage/AnnotationDatabaseTests.cs
+++ src/Statsify.Tests/Core/Storage/AnnotationDatabaseTests.cs
@@ -13,17 +13,13 @@ namespace Statsify.Tests.Core.Storage
          {
             var path = Path.GetTempFileName();
 
-            var annotationDatabase = AnnotationDatabase.Create(@"c:\statsify\annotations.db");
+            var annotationDatabase = AnnotationDatabase.Create(path);
             var now = DateTime.UtcNow.AddHours(-2);
 
             for(var i = 0; i < 10; ++i)
             {
-                annotationDatabase.WriteAnnotation(now.AddSeconds(i), "Deployment", "Deployed changeset cf43450d", "aeroclub-time", "deployment", "production");
-                annotationDatabase.WriteAnnotation(now.AddSeconds(i * 2), "Deployment Started",
-                    string.Format("Started deployment of Aeroclub Platform Backend - Production (Build {0}).\r\n\r\n" +
-                                  "Changes:\r\n" +
-                                  "http://dev.aeroclub.int/hglab/projects/time/repositories/aeroclub.time/compare/3b40122d48...c5162e7c83", i),
-                                  "aeroclub-time", "deployment", "staging", "rollback");
+                annotationDatabase.WriteAnnotation(now.AddSeconds(i), "Deployment", "Deployed changeset cf43450d", "platform-backend", "deployment", "production");
+                annotationDatabase.WriteAnnotation(now.AddSeconds(i * 2), "Deployment Started", "Started deployment of Platform Backend", "deployment", "staging", "rollback");
             } // for
 
             var annotations = annotationDatabase.ReadAnnotations(now, now.AddMinutes(1));
