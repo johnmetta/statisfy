e24b0647f6128db2f8de748f1c88198b37a29acb
Anton Gogolev <anton.gogolev@aeroclub.ru>
2015-05-26 15:51:35 +0300

    Core: Move `BinaryReaderExtensions` out of `AnnotationDatabase.cs`

--- src/Statsify.Core/Storage/AnnotationDatabase.cs
+++ src/Statsify.Core/Storage/AnnotationDatabase.cs
@@ -171,40 +171,4 @@ namespace Statsify.Core.Storage
         }
 
     }
-
-    internal static class BinaryReaderExtensions
-    {
-        public static bool TryRead(this BinaryReader binaryReader, int bytes, out byte[] value)
-        {
-            return TryRead(binaryReader, br => br.ReadBytes(bytes), bytes, out value);
-        }
-
-        public static bool TryReadInt32(this BinaryReader binaryReader, out int value)
-        {
-            return TryRead(binaryReader, br => br.ReadInt32(), sizeof(int), out value);
-        }
-
-        public static bool TryReadInt64(this BinaryReader binaryReader, out long value)
-        {
-            return TryRead(binaryReader, br => br.ReadInt64(), sizeof(long), out value);
-        }
-
-        private static bool TryRead<T>(this BinaryReader binaryReader, Func<BinaryReader, T> reader, int bytes, out T value)
-        {
-            if(!Buff(binaryReader, bytes))
-            {
-                value = default(T);
-                return false;
-            } // if
-
-            value = reader(binaryReader);
-            return true;
-        }
-
-        private static bool Buff(BinaryReader binaryReader, int bytes)
-        {
-            var stream = binaryReader.BaseStream;
-            return stream.Position + bytes <= stream.Length;
-        }
-    }
 }
