af7ec73cd36fe4b1248b71003fe3c3a3f0a21b26
Anton Gogolev <anton.gogolev@gmail.com>
2015-06-01 13:14:06 +0300

    Agent: Rewrite Performance Counter parsing logic

--- src/Statsify.Tests/Agent/Impl/MetricDefinitionFactoryTests.cs
+++ src/Statsify.Tests/Agent/Impl/MetricDefinitionFactoryTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
 using Statsify.Agent.Configuration;
 using Statsify.Agent.Impl;
 
@@ -16,6 +18,39 @@ namespace Statsify.Tests.Agent.Impl
                 Path = @"\SQLServer:Locks(_Total)\Lock Timeouts/sec",
                 AggregationStrategy = AggregationStrategy.Gauge
             });
+        }
+
+        [Test]
+        public void ParsePerformanceCounterDefinition()
+        {
+            string machineName;
+            string categoryName;
+            string instanceName;
+            string counterName;
+
+            MetricDefinitionFactory.ParsePerformanceCounterDefinition(@"\\Local-Host\System\Threads",
+                out machineName, out categoryName, out instanceName, out counterName);
+
+            Assert.AreEqual("Local-Host", machineName);
+            Assert.AreEqual("System", categoryName);
+            Assert.IsNull(instanceName);
+            Assert.AreEqual("Threads", counterName);
+
+            MetricDefinitionFactory.ParsePerformanceCounterDefinition(@"\\Local-Host\LogicalDisk(C:)\Disk Read Bytes/sec",
+                out machineName, out categoryName, out instanceName, out counterName);
+
+            Assert.AreEqual("Local-Host", machineName);
+            Assert.AreEqual("LogicalDisk", categoryName);
+            Assert.AreEqual("C:", instanceName);
+            Assert.AreEqual("Disk Read Bytes/sec", counterName);
+
+            MetricDefinitionFactory.ParsePerformanceCounterDefinition(@"\LogicalDisk(C:)\Disk Read Bytes/sec",
+                out machineName, out categoryName, out instanceName, out counterName);
+
+            Assert.IsNull(machineName);
+            Assert.AreEqual("LogicalDisk", categoryName);
+            Assert.AreEqual("C:", instanceName);
+            Assert.AreEqual("Disk Read Bytes/sec", counterName);
         }
     }
 }
