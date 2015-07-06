d708cf67637c9c3cff2fdac377e70ebe549e61a7
Anton Gogolev <anton.gogolev@aeroclub.ru>
2015-07-06 07:11:37 +0300

    Core: Add `Timestamp` struct

---
+++ src/Statsify.Core/Storage/Timestamp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Statsify.Core.Storage
+{
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public struct Timestamp
+    {
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        private readonly long ticks;
+
+        // ReSharper disable once UnusedMember.Local
+        private string DebuggerDisplay
+        {
+            get { return ((DateTime)this).ToString("HH:mm:ss dd.MM.yyyy"); }
+        }
+
+        public Timestamp(long ticks)
+        {
+            this.ticks = ticks;
+        }
+
+        public static implicit operator Timestamp(long ticks)
+        {
+            return new Timestamp(ticks);
+        }
+
+        public static implicit operator Timestamp(DateTime dateTime)
+        {
+            var elapsedTime = dateTime - Epoch;
+            return (long)elapsedTime.TotalSeconds;
+        }
+
+        public static implicit operator long(Timestamp timestamp)
+        {
+            return timestamp.ticks;
+        }
+
+        public static implicit operator DateTime(Timestamp timestamp)
+        {
+            return Epoch.AddSeconds(timestamp.ticks);
+        }
+    }
+}
