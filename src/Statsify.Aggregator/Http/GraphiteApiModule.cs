b631a5478727865a0cd142e3041b447b741251c3
Anton Gogolev <anton.gogolev@aeroclub.ru>
2015-09-23 09:37:55 +0300

    Working on Graphite compatibility

--- src/Statsify.Aggregator/Http/GraphiteApiModule.cs
+++ src/Statsify.Aggregator/Http/GraphiteApiModule.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using Nancy;
-using Nancy.Helpers;
 using Nancy.ModelBinding;
 using Statsify.Aggregator.ComponentModel;
 using Statsify.Aggregator.Http.Models;
+using Statsify.Aggregator.Http.Services;
 using Statsify.Core.Components;
 using Statsify.Core.Expressions;
 using Statsify.Core.Util;
 
-namespace Statsify.Aggregator.Http.Services
+namespace Statsify.Aggregator.Http
 {
     /// <summary>
-    /// See https://github.com/brutasse/graphite-api/blob/master/docs/api.rst
+    /// See:
+    /// * https://github.com/brutasse/graphite-api/blob/master/docs/api.rst
+    /// * http://graphite-api.readthedocs.org/en/latest/api.html
     /// </summary>
     public class GraphiteApiModule : NancyModule
     {
@@ -28,19 +30,20 @@ namespace Statsify.Aggregator.Http.Services
             this.metricRegistry = metricRegistry;
             this.metricAggregator = metricAggregator;
 
-            Get["/metrics/find"] = GetMetrics;
-            Get["/metrics"] = GetMetrics;
-            Post["/render"] = GetSeries;
+            Get["/metrics/find"] = Get["/metrics"] = Post["/metrics/find"] = Post["/metrics"] = QueryMetrics;
+            Get["/render"] = Post["/render"] = RenderSeries;
         }
 
-
-        private object GetMetrics(dynamic r)
+        private object QueryMetrics(dynamic r)
         {
-            var model = new MetricsQueryModel();
+            var model = new QueryMetricsModel();
             this.BindTo(model, new BindingConfig { BodyOnly = false });
 
             if(string.IsNullOrWhiteSpace(model.Query))
                 return HttpStatusCode.BadRequest;
+
+            if((model.Format ?? "").ToLowerInvariant() == "treejson")
+                return new Response { StatusCode = HttpStatusCode.BadRequest, ReasonPhrase = "'treejson' format is not currently supported" };
 
             var now = DateTime.UtcNow;
             var from = Parser.ParseDateTime(model.From, now, now.AddHours(-1));
@@ -48,41 +51,31 @@ namespace Statsify.Aggregator.Http.Services
 
             var metrics = 
                 metricService.Find(model.Query).
-                    Select(m => new {
+                    Select(m => new QueryMetricsResultModel {
                         is_leaf = m.IsLeaf ? 1 : 0,
                         name = m.Name,
                         path = m.IsLeaf ? m.Path.TrimEnd('.') : (m.Path.TrimEnd('.') + '.')
-                    });
+                    }).
+                    ToList();
 
-            return Response.AsJson(new { metrics = metrics.ToArray() });
+            if(model.Wildcards == 1)
+                metrics.Add(new QueryMetricsResultModel { name = "*" });
+
+            return Response.AsJson(new { metrics = metrics });
         }
 
-            public class GraphiteRenderModel
-    {
-        public string From { get; set; }
-
-        public string Until { get; set; }
-
-        public string Target { get; set; }
-    }
-
-
-        private dynamic GetSeries(dynamic req)
+        private dynamic RenderSeries(dynamic req)
         {
             try
             {
-                var model = new GraphiteRenderModel();
+                var model = new RenderSeriesModel();
                 this.BindTo(model, new BindingConfig { BodyOnly = false });
-
-                //
-                // FIXME: Nancy
 
                 var now = DateTime.UtcNow;
                 var from = Parser.ParseDateTime(model.From, now, now.AddHours(-1));
                 var until = Parser.ParseDateTime(model.Until, now, now);
 
-                var environment = new Statsify.Core.Expressions.Environment
-                {
+                var environment = new Statsify.Core.Expressions.Environment {
                     MetricRegistry = metricRegistry,
                     QueuedMetricDatapoints = metricAggregator.Queue
                 };
@@ -90,23 +83,20 @@ namespace Statsify.Aggregator.Http.Services
                 var evalContext = new EvalContext(@from, until);
 
                 var metrics = new List<Core.Model.Metric>();
-                //foreach(var expression in model.Target)
+                var scanner = new ExpressionScanner();
+                var tokens = scanner.Scan(model.Target);
+
+                var parser = new ExpressionParser();
+                var e = parser.Parse(new TokenStream(tokens));
+
+                if(e is MetricSelectorExpression)
                 {
-                    var scanner = new ExpressionScanner();
-                    var tokens = scanner.Scan(model.Target);
+                    e = new EvaluatingMetricSelectorExpression(e as MetricSelectorExpression);
+                } // if
 
-                    var parser = new ExpressionParser();
-                    var e = parser.Parse(new TokenStream(tokens));
+                var r = (Core.Model.Metric[])e.Evaluate(environment, evalContext);
 
-                    if(e is MetricSelectorExpression)
-                    {
-                        e = new EvaluatingMetricSelectorExpression(e as MetricSelectorExpression);
-                    } // if
-
-                    var r = (Core.Model.Metric[])e.Evaluate(environment, evalContext);
-
-                    metrics.AddRange(r);
-                } // foreach
+                metrics.AddRange(r);
 
                 var seriesViewList = (from metric in metrics
                     let f = metric.Series.From.ToUnixTimestamp()
@@ -125,15 +115,31 @@ namespace Statsify.Aggregator.Http.Services
                 return Response.AsJson(new {e.Message, e.StackTrace}, HttpStatusCode.InternalServerError);
             }
         }
-    }
 
-    public class MetricsQueryModel
-    {
-        public string Query { get; set; }
-        public string Format { get; set; }
-        public int Wildcards { get; set; }
-        public string From { get; set; }
-        public string Until { get; set; }
-        public string Jsonp { get; set; }
+        public class RenderSeriesModel
+        {
+            public string From { get; set; }
+            public string Until { get; set; }
+            public string Target { get; set; }
+        }
+
+        public class QueryMetricsModel
+        {
+            public string Query { get; set; }
+            public string Format { get; set; }
+            public int Wildcards { get; set; }
+            public string From { get; set; }
+            public string Until { get; set; }
+            public string Jsonp { get; set; }
+        }
+
+        public class QueryMetricsResultModel
+        {
+            // ReSharper disable InconsistentNaming
+            public int is_leaf { get; set; }
+            public string name { get; set; }
+            public string path { get; set; }
+            // ReSharper restore InconsistentNaming
+        }
     }
 }
