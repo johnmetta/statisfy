07df962e4bd975d7586b15c931479926c479e4cd
Anton Gogolev <anton.gogolev@aeroclub.ru>
2015-12-14 15:27:34 +0300

    Agent: Add `MetricSource` and `MetricSourceFactory` abstractions

---
+++ src/Statsify.Agent/Impl/MetricSource.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Statsify.Agent.Impl
+{
+    public class MetricSource : IMetricSource
+    {
+        private readonly IList<IMetricDefinition> metricDefinitions;
+
+        public MetricSource(IEnumerable<IMetricDefinition> metricDefinitions)
+        {
+            this.metricDefinitions = new List<IMetricDefinition>(metricDefinitions ?? Enumerable.Empty<IMetricDefinition>());
+        }
+
+        public IEnumerable<IMetricDefinition> MetricDefinitions
+        {
+            get { return metricDefinitions; }
+        }
+    }
+}
