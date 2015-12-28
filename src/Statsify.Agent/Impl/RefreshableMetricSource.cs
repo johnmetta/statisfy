ec95ccc7ccfbb142ab8d6a7176c9ff64018b98cc
Anton Gogolev <anton.gogolev@aeroclub.ru>
2015-12-28 20:22:17 +0300

    Merge with `default`

---
+++ src/Statsify.Agent/Impl/RefreshableMetricSource.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using NLog;
+using Statsify.Agent.Util;
+
+namespace Statsify.Agent.Impl
+{
+    public class RefreshableMetricSource : IMetricSource
+    {
+        private readonly Logger log = LogManager.GetCurrentClassLogger();
+        private IList<IMetricDefinition> metricDefinitions;
+        private DateTime lastRefreshedAt = DateTime.UtcNow;
+        private bool refreshing;
+        private readonly TimeSpan refreshEvery;
+        private readonly Func<IEnumerable<IMetricDefinition>> refreshCallback;
+        private readonly Func<DateTime> currentTimeProvider;
+        private readonly ReaderWriterLockSlim readerWriterLock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+
+        public RefreshableMetricSource(IEnumerable<IMetricDefinition> metricDefinitions, TimeSpan refreshEvery, Func<IEnumerable<IMetricDefinition>> refreshCallback) :
+            this(metricDefinitions, refreshEvery, refreshCallback, () => DateTime.UtcNow)
+        {
+        }
+
+        internal RefreshableMetricSource(IEnumerable<IMetricDefinition> metricDefinitions, TimeSpan refreshEvery, Func<IEnumerable<IMetricDefinition>> refreshCallback, Func<DateTime> currentTimeProvider)
+        {
+            this.metricDefinitions = new List<IMetricDefinition>(metricDefinitions ?? Enumerable.Empty<IMetricDefinition>());
+            this.refreshEvery = refreshEvery;
+            this.refreshCallback = refreshCallback;
+            this.currentTimeProvider = currentTimeProvider;
+        }
+
+        public IEnumerable<IMetricDefinition> GetMetricDefinitions()
+        {
+            IList<IMetricDefinition> definitions;
+            
+            using(readerWriterLock.AcquireReadLock())
+                definitions = metricDefinitions.ToList();
+
+            TryRefreshMetricDefinitions();
+
+            return definitions;
+        }
+
+        public void InvalidateMetricDefinition(IMetricDefinition metricDefinition)
+        {
+            using(readerWriterLock.AcquireWriteLock())
+                metricDefinitions.Remove(metricDefinition);
+        }
+
+        private void TryRefreshMetricDefinitions()
+        {
+            if(lastRefreshedAt + refreshEvery > currentTimeProvider()) return;
+
+            using(readerWriterLock.AcquireUpgradeableReadLock())
+            {
+                if(refreshing) return;
+
+                using(readerWriterLock.AcquireWriteLock())
+                {
+                    if(refreshing) return;
+                    refreshing = true;
+                } // using
+            } // using
+
+            log.Trace("attempting to refresh metric definitions");
+            
+            ThreadPool.QueueUserWorkItem(s =>
+            {
+                var stopwatch = Stopwatch.StartNew();
+                var definitions = refreshCallback();
+
+                using(readerWriterLock.AcquireWriteLock())
+                {
+                    metricDefinitions = new List<IMetricDefinition>(definitions ?? Enumerable.Empty<IMetricDefinition>());
+                    lastRefreshedAt = currentTimeProvider();
+                    refreshing = false;
+                } // using
+
+                stopwatch.Stop();
+
+                log.Trace("completed refreshing metric definitions in {0}", stopwatch.Elapsed);
+            });
+        }
+    }
+}
---
+++ src/Statsify.Agent/Util/ReaderWriterLockSlim.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace Statsify.Agent.Util
+{
+    internal static class ReaderWriterLockSlimExtensions
+    {
+        internal static IDisposable AcquireReadLock(this ReaderWriterLockSlim readerWriterLock)
+        {
+            readerWriterLock.EnterReadLock();
+            return new ReaderWriterLockToken(readerWriterLock.ExitReadLock);
+        }
+
+        internal static IDisposable AcquireWriteLock(this ReaderWriterLockSlim readerWriterLock)
+        {
+            readerWriterLock.EnterWriteLock();
+            return new ReaderWriterLockToken(readerWriterLock.ExitWriteLock);
+        }
+
+        internal static IDisposable AcquireUpgradeableReadLock(this ReaderWriterLockSlim readerWriterLock)
+        {
+            readerWriterLock.EnterUpgradeableReadLock();
+            return new ReaderWriterLockToken(readerWriterLock.ExitUpgradeableReadLock);
+        }
+
+            internal sealed class ReaderWriterLockToken : IDisposable
+        {
+            private readonly Action releaseCallback;
+
+            public ReaderWriterLockToken(Action releaseCallback)
+            {
+                this.releaseCallback = releaseCallback;
+            }
+
+            public void Dispose()
+            {
+                releaseCallback();
+            }
+        }
+    }
+}
