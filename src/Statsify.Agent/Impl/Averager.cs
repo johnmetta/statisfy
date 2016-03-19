0eff1982f75241f76503bc3edad645c1756f8973
Anton Gogolev <anton.gogolev@aeroclub.ru>
2016-03-19 21:07:24 +0300

    Agent: `Averager`

---
+++ src/Statsify.Agent/Impl/Averager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Statsify.Agent.Impl
+{
+    public class Averager
+    {
+        private readonly IDictionary<string, Measurement> measurements = 
+            new Dictionary<string, Measurement>();
+
+        private class Measurement
+        {
+            public int Values { get; private set; }
+            public double AverageValue { get; private set; }
+            public double LastValue { get; private set; }
+
+            public void Record(double value)
+            {
+                Values += 1;
+                AverageValue = AverageValue + (value - AverageValue) / Values;
+                LastValue = value;
+            }
+        }
+
+        public class Outlier
+        {
+            public string Name { get; private set; }
+            public double AverageValue { get; private set; }
+            public double LastValue { get; private set; }
+
+            public Outlier(string name, double lastValue, double averageValue)
+            {
+                Name = name;
+                LastValue = lastValue;
+                AverageValue = averageValue;
+            }
+        }
+        
+        public IList<Outlier> GetOutliers(int minValues)
+        {
+            var outliers = 
+                measurements.
+                    Where(m => m.Value.Values >= minValues).
+                    Where(m => m.Value.LastValue > m.Value.AverageValue).
+                    Select(m => new Outlier(m.Key, m.Value.LastValue, m.Value.AverageValue)).
+                    ToList();
+
+            return outliers;
+        }
+
+        public void Record(string metric, double value)
+        {
+            Measurement measurement;
+            if(!measurements.TryGetValue(metric, out measurement))
+            {
+                measurement = new Measurement();
+                measurements.Add(metric, measurement);
+            } // if
+
+            measurement.Record(value);
+        }
+    }
+}
