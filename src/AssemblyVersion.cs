a9bcb0e890f05ad72d8db1a2b263564fdb153f1b
Anton Gogolev <anton.gogolev@gmail.com>
2016-04-19 00:33:34 +0300

    v1.1.0

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -1,3 +1,3 @@
 [assembly:System.Reflection.AssemblyVersion("1.0.0.0")]
-[assembly:System.Reflection.AssemblyFileVersion("1.0.2")]
-[assembly:System.Reflection.AssemblyInformationalVersion("1.0.2")]
+[assembly:System.Reflection.AssemblyFileVersion("1.1.0")]
+[assembly:System.Reflection.AssemblyInformationalVersion("1.1.0")]
