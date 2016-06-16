2ff4f674f2595df8ae881bc01e4c2fe0d71f0853
Anton Gogolev <anton.gogolev@aeroclub.ru>
2016-06-16 13:55:29 +0300

    v1.2.0

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -1,3 +1,3 @@
 [assembly:System.Reflection.AssemblyVersion("1.0.0.0")]
-[assembly:System.Reflection.AssemblyFileVersion("1.1.0")]
-[assembly:System.Reflection.AssemblyInformationalVersion("1.1.0")]
+[assembly:System.Reflection.AssemblyFileVersion("1.2.0")]
+[assembly:System.Reflection.AssemblyInformationalVersion("1.2.0")]
