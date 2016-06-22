47363803dd29daa301df66fa4620a8a6a23ca3ff
Anton Gogolev <anton.gogolev@aeroclub.ru>
2016-06-22 14:43:07 +0300

    Aggregator: Major performance improvements in `MetricParser.ParseMetric()`

--- src/Statsify.Aggregator/MetricParser.cs
+++ src/Statsify.Aggregator/MetricParser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using NLog;
 using Statsify.Aggregator.Extensions;
 
@@ -10,7 +11,11 @@ namespace Statsify.Aggregator
 {
     public class MetricParser
     {
-        private readonly Logger log = LogManager.GetCurrentClassLogger();      
+        private readonly Logger log = LogManager.GetCurrentClassLogger();
+        private readonly IDictionary<string, string> metricNameCache = new Dictionary<string, string>();
+        private static readonly Regex SlashesRegex = new Regex(@"[\/|\\]", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+        private static readonly Regex NonAlphanumericRegex = new Regex(@"[^a-zA-Z_\-0-9\.]", RegexOptions.Compiled);
+        private static readonly Regex WhitespaceRegex = new Regex(@"\s+", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
 
         public IEnumerable<Metric> ParseMetrics(byte[] buffer)
         {
@@ -31,16 +36,21 @@ namespace Statsify.Aggregator
         public Metric ParseMetric(string metric)
         {
             if(string.IsNullOrWhiteSpace(metric)) return null;
-
+            
             var bits = metric.Split(':');
 
-            var name = 
-                bits[0].
-                    RegexReplace(@"\s+", "_").
-                    RegexReplace(@"\/", "-").
-                    RegexReplace(@"\\", "-").
-                    RegexReplace(@"[^a-zA-Z_\-0-9\.]", String.Empty).
+            string name;
+
+            if(!metricNameCache.TryGetValue(bits[0], out name))
+            {
+                name = bits[0].
+                    RegexReplace(WhitespaceRegex, "_").
+                    RegexReplace(SlashesRegex, "-").
+                    RegexReplace(NonAlphanumericRegex, String.Empty).
                     Trim('.');
+
+                metricNameCache[bits[0]] = name;
+            } // if
 
             var fields = bits[1].Split('|');
 
