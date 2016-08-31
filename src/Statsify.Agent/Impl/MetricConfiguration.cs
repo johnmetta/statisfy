21083def90f49e2e1a0b4a3a1f79946070636998
Anton Gogolev <anton.gogolev@aeroclub.ru>
2016-08-31 10:42:07 +0300

    Agent: Add `IMetricConfiguration` interface

---
+++ src/Statsify.Agent/Impl/MetricConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Statsify.Agent.Configuration;
+
+namespace Statsify.Agent.Impl
+{
+    public class MetricConfiguration : IMetricConfiguration
+    {
+        public string Name { get; private set; }
+
+        public string Type { get; private set; }
+
+        public string Path { get; private set; }
+
+        public AggregationStrategy AggregationStrategy { get; private set; }
+
+        public TimeSpan? RefreshEvery { get; private set; }
+
+        public MetricConfiguration(string name, string type, string path, AggregationStrategy aggregationStrategy, TimeSpan? refreshEvery)
+        {
+            Name = name;
+            Type = type;
+            Path = path;
+            AggregationStrategy = aggregationStrategy;
+            RefreshEvery = refreshEvery;
+        }
+    }
+}
