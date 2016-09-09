cf537409799f1c183638f4a879e50f2844169310
Anton Gogolev <anton.gogolev@aeroclub.ru>
2016-09-09 13:41:14 +0300

    v1.8.0

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -1,3 +1,3 @@
 [assembly:System.Reflection.AssemblyVersion("1.0.0.0")]
-[assembly:System.Reflection.AssemblyFileVersion("1.7.3")]
-[assembly:System.Reflection.AssemblyInformationalVersion("1.7.3")]
+[assembly:System.Reflection.AssemblyFileVersion("1.8.0")]
+[assembly:System.Reflection.AssemblyInformationalVersion("1.8.0")]
