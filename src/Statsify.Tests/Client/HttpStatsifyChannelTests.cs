5eb7a1458b387c4b49048e041e475d7bf2b01ba7
Anton Gogolev <anton.gogolev@aeroclub.ru>
2016-12-03 12:41:57 +0300

    Client: More smarts in `HttpStatsifyChannel`

---
+++ src/Statsify.Tests/Client/HttpStatsifyChannelTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using Statsify.Client;
+
+namespace Statsify.Tests.Client
+{
+    [TestFixture]
+    public class HttpStatsifyChannelTests
+    {
+        [Test]
+        [TestCase("http://statsify.int", Result = "http://statsify.int/api/v1/metrics")]
+        [TestCase("http://statsify.int/", Result = "http://statsify.int/api/v1/metrics")]
+        [TestCase("http://localhost/statsify", Result = "http://localhost/statsify/api/v1/metrics")]
+        [TestCase("http://localhost/statsify/", Result = "http://localhost/statsify/api/v1/metrics")]
+        [TestCase("http://localhost/statsify/api", Result = "http://localhost/statsify/api/v1/metrics")]
+        [TestCase("http://localhost/statsify/api/v1/", Result = "http://localhost/statsify/api/v1/metrics")]
+        public string GetPostUri(string uri)
+        {
+            var statsifyAggregatorApiUri = HttpStatsifyChannel.GetPostUri(new Uri(uri));
+            return statsifyAggregatorApiUri.ToString();
+        }
+    }
+}
