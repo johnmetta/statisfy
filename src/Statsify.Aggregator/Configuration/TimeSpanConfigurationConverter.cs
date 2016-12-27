607b1b7d16e921617ed5ffd708d817dc8fdcb807
Anton Gogolev <anton.gogolev@aeroclub.ru>
2016-12-27 15:06:31 +0300

    Aggregator: Basic configuration for Apdex

---
+++ src/Statsify.Aggregator/Configuration/TimeSpanConfigurationConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Globalization;
+
+namespace Statsify.Aggregator.Configuration
+{
+    public class TimeSpanConfigurationConverter : ConfigurationConverterBase
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext ctx, Type type)
+        {
+            return type == typeof(string);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            return RetentionConfigurationElement.ParseTimeSpan((string)value);
+        }
+    }
+}
